03644b3e99fe3fb8f17d7597be9a1508b835cba0
Alexander <alex@kali.kali>
2026-02-26 19:06:32 -0600

    Dynamic

--- src/app/category/[id]/page.tsx
+++ src/app/category/[id]/page.tsx
@@ -1,6 +1,9 @@
 import { Suspense } from 'react'
 import CategoryContent from './CategoryContent'
 
+
+export const dynamic = 'force-dynamic'
+
 export default function Page() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,6 +1,8 @@
 import { Suspense } from 'react'
 import SearchContent from './SearchContent'
 
+export const dynamic = 'force-dynamic'
+
 export default function Page() {
   return (
     <Suspense fallback={<div className="p-10 text-white/40">Loading...</div>}>
